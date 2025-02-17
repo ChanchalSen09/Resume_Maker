1fb17b7a1c8b56b0a80fc607f996d9fe5ae67cb9
chanchalvex <chanchal@devnex.in>
2025-02-17 23:44:22 +0530

    --

---
+++ data/testimonial.js
@@ -0,0 +1,26 @@
+export const testimonial = [
+  {
+    quote:
+      "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
+    author: "Chanchal Sen",
+    image: "https://randomuser.me/api/portraits/men/75.jpg",
+    role: "React Developer",
+    company: "DAPS Software Pvt. Ltd",
+  },
+  {
+    quote:
+      "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
+    author: "Aditya Verma",
+    image: "https://randomuser.me/api/portraits/men/75.jpg",
+    role: "Software Engineer",
+    company: "Peppyhop",
+  },
+  {
+    quote:
+      "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
+    author: "Samroj Khan",
+    image: "https://randomuser.me/api/portraits/men/74.jpg",
+    role: "Graffic Designer",
+    company: "Freelancer",
+  },
+];
